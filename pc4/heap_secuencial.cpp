d6a5e01a4e14c1af1771edd22f33decb4bd61a5a
Juan Carlos Espejo Delzo <jespejod@uni.edu.pe>
2017-01-06 23:13:18 +0000

    Add files via upload

---
+++ pc4/heap_secuencial.cpp
@@ -0,0 +1,277 @@
+//----------------- DECLARACION DE LAS LIBRERIRAS QUE NECESITAREMOS --------------// 
+
+
+#include <ctime>
+#include <math.h>
+#include <iostream.h>
+#include <iostream>
+using std::cerr;
+using std::cout;
+using std::cin;
+using std::endl;
+
+#include <fstream>
+using std::ifstream;
+
+#include <cstdlib> // for exit function
+
+    // This program reads values from the file 'example.dat'
+    // and echoes them to the display until a negative value
+    // is read.
+
+//----------------------------- FIN DE LA DECLARACION ----------------------------//
+
+
+
+//-------------------- DECLARACION DE LAS VARIABLES GLOBALES ---------------------//
+    
+int H[100]; // Arreglo de enteros que almacenara los mismos ingresados
+int cont=0; // Contador de los intercambios de elementos del vector A
+
+//----------------------------- FIN DE LA DECLARACION -----------------------------//
+
+
+
+//-------------------- DECLARACION DE LAS FUNCIONES EMPLEADAS ---------------------//
+
+int  Crear_heap (int n);
+
+void Gracias ();
+
+void Heapsort (int n);
+
+int  Ingresar ();
+void Imprimir (int n);
+int  Insertar (int x, int n);
+int  Ingresar_opcion ();
+
+void Mostrar_menu ();
+
+void Programa_a ();
+void Programa_b ();
+
+int  Remover (int n);
+
+//------------------------------- FIN DE LA DECLARACION -----------------------------//
+
+
+
+//-------------------------------- INICIO DEL PROGRAMA ------------------------------//
+
+int main()
+  {
+	int opcion = 0; 
+	bool salir = false;
+		
+	while (!salir)
+	    {
+		   Mostrar_menu ();
+		   opcion = Ingresar_opcion ();
+		   switch (opcion)
+                {
+			      case 1 : {
+							   Programa_a ();
+			               } 
+			               break;
+			      case 2 : {
+							   Programa_b ();
+						   } 
+						   break;
+				  case 3 : {
+					           Gracias ();  
+							   salir = true; 
+						   } 
+						   break;
+			    }
+		 }
+
+	return 0;
+  }
+  
+//---------------------------------- FIN DEL PROGRAMA -------------------------------//
+
+
+
+// ----------------------- DEFINICION DE LAS FUNCIONES EMPLEADAS --------------------//
+
+int Crear_heap (int n)
+   {
+	 int N=0;  
+	 for (int i=1 ; i<=n ; i++)
+	   	 N = Insertar (H[i],N);  
+	 return N;
+   }
+
+void Gracias ()
+     {
+	    printf("\n\n");
+	    printf("\t==================== GRACIAS ======================\n");	 
+	 }
+	 
+void Heapsort (int n)
+   {
+     int N = Crear_heap (n);
+	 for (int i=1 ; i<=n ; i++)
+	   {
+		 int temp = H[1];
+		 N = Remover (N);
+		 H[N+1] = temp;
+	   }
+   }
+ 
+int Insertar (int x, int n)
+  { 
+	n++; 
+	int son = n;
+	int dad = son/2;
+	bool up = true;
+	while ( (dad>0) && (up) )
+	    {
+		  if (H[dad] >= x)
+		     up = false;
+		  else
+		   {
+		     H[son] = H[dad];
+		     H[dad] = x;
+		     son = dad;
+		     dad = son/2;
+		     cont++;
+		   }  	
+		} 
+	H[son] = x;
+	return n;
+  }
+
+int Ingresar ()
+  {
+	int n;   
+    cout << "\tIngrese el tamanio, n, del vector A que va ordenar.\n\n\tn: ";
+    cin >> n; 
+    cout << "\n\tIngrese cada elemento de A presionando enter por cada uno.\n\n";
+    for (int i=1 ; i<=n ; i++)
+      {
+		printf("\tA[%d] = ",i);
+		cin >> H[i];
+		cout << "\n";  
+	  }
+	cout << "\n\n"; 
+	return n; 
+  }
+   
+void Imprimir (int n)
+   {
+	 cout << "\t";  
+	 for (int i=1 ; i<=n ; i++)
+	     cout << H[i] << " ";
+	 cout << "\n\n";
+   } 
+   
+int  Ingresar_opcion ()
+  {
+	 char opcion[100], car;
+	 int N=0, op=0;
+	
+     while (op == 0)
+         {
+	       printf("\tIngrese su opcion y presione enter: ");	
+		   while ( ( car = getchar() ) != '\n' )
+		         opcion[N++] = car;
+		   if (N == 1)
+			{ 
+			  car = opcion[0];
+			  switch (car)
+			  {
+			   case 'A' : op = 1;
+			              break;
+		       case 'B' : op = 2; 
+						  break;
+		       case 'S' : op = 3; 
+						  break;
+		       default  : op = 0;
+			  }
+		    }
+	       N = 0;
+	       printf("\n\n");
+	     }
+	 return op;
+  }   
+  
+void Mostrar_menu ()
+	 {
+      printf("\n\n");
+	  printf("\t###################################################\n");
+	  printf("\t#                                                 #\n"); 
+	  printf("\t#        ===============================          #\n"); 
+      printf("\t#                   PROGRAMA :                    #\n");
+	  printf("\t#        PREGUNTA 3 DE LA 4ta CALIFICADA          #\n");
+	  printf("\t#        ===============================          #\n");
+	  printf("\t#                                                 #\n");  
+	  printf("\t#                     MENU:                       #\n");
+	  printf("\t#                                                 #\n");
+	  printf("\t#         (A)  :  Subprograma (a)                 #\n");
+	  printf("\t#         (B)  :  Subprograma (b)                 #\n");
+	  printf("\t#         (S)  :  Salir                           #\n");
+	  printf("\t#                                                 #\n");
+	  printf("\t###################################################\n");
+	  printf("\n\n\n");
+     }
+      
+void Programa_a ()
+   {
+	 int n = Ingresar ();
+	 cout << "\tArreglo ingresado:\n\n";
+	 Imprimir (n);
+	 Heapsort (n);
+	 cout << "\n\n";
+	 cout << "\tArreglo ordenado:\n\n";
+	 Imprimir (n); 
+	 cont = 0; 
+   } 
+
+void Programa_b ()
+   {
+	 srand((unsigned)time(0));
+     int T,n; 
+     double P,Q;
+     
+	 for (int k=2 ; k<10 ; k=k+2)
+	   {
+		 n = pow(2,k);    
+         for (int i=1; i<=n ; i++)
+             H[i] = (rand()%1000) + 1;
+         Heapsort (n);
+         T = cont;
+         cont = 0;
+         P=T ; Q=n*k;      
+         printf("\tn = %d \t\t T = %d \t\t T/(n*log_{2}n) = %4.2f \n\n",n,T,P/Q);
+       }
+   } 
+
+int  Remover (int n)
+  {
+	 int aux = H[n];
+	 int dad = 1;
+	 int son = 2;
+	 bool down = true;
+	 while ( (son<=n) && (down) )
+	    {
+	      if (son<n)
+	       {
+			 if (H[son] < H[son+1])
+			    ++son;  
+		   }
+		  if (H[son] <= aux)
+		     down = false;
+		  else
+		   {
+			 H[dad] = H[son];
+			 dad = son;
+			 son = 2*son;
+			 cont++;
+		   } 
+		}
+	 H[dad] = aux;
+	 return --n;
+  }
+
+//---------------------------- FIN DE LA DEFINICION ---------------------------------//
---
+++ pc4/jc.cpp
@@ -0,0 +1,344 @@
+//----------------- DECLARACION DE LAS LIBRERIRAS QUE NECESITAREMOS --------------// 
+
+						#include <stdio.h>
+						#include <iostream>
+						 using std::cin;
+
+//----------------------------- FIN DE LA DECLARACION ----------------------------//
+
+
+
+
+//-------------------- DEFINICION DE LA ESTRUCTURA DE UN NODO --------------------//
+
+		typedef struct Nodo 
+			{
+				char info;   // Caracter que almacena el nodo
+				Nodo *sig;  // Direccion del nodo siguiente
+				Nodo *ant;  // Direccion del nodo anterior
+			} 
+		Nodo;
+
+//--------------------------- FIN DE LA DEFINICION -------------------------------//
+
+
+
+
+//-------------------- DECLARACION DE LAS VARIABLES GLOBALES ---------------------//
+
+	// Definimos las listas que usaremos:
+	// cabeza y nodo de la lista que almacenara la cadena de caracteres ingresada
+		Nodo *cabeza; 
+		Nodo *cola;
+	// cabeza y nodo de la listaR que almacenara el resultado despues de la 
+	// primera etapa
+		Nodo *cabezaR;
+		Nodo *colaR;
+	    char pila[100];
+	    char cad[100];	
+
+//----------------------------- FIN DE LA DECLARACION -----------------------------//
+
+
+
+
+//-------------------- DECLARACION DE LAS FUNCIONES EMPLEADAS ---------------------//
+
+void Agregar_nodo (char car); 
+void Agregar_nodoR (char car);
+
+void Borrar_cadena ();
+void Borrar_resultado ();
+
+bool Es_vocal (char car);
+
+void Gracias ();
+
+void Ingresar_cadena ();
+int  Ingresar_opcion ();
+void Iniciar_listas ();
+void Invertir ();
+
+void Mostrar_cadena ();
+void Mostrar_menu ();
+void Mostrar_resultado ();
+
+void Program_enc ();
+	                      
+//------------------------------- FIN DE LA DECLARACION -----------------------------//
+
+
+
+
+//-------------------------------- INICIO DEL PROGRAMA ------------------------------//
+		
+int main() 
+  {
+	Program_enc ();
+	return 0;
+  }
+    
+//---------------------------------- FIN DEL PROGRAMA -------------------------------//
+
+
+
+
+// ----------------------- DEFINICION DE LAS FUNCIONES EMPLEADAS --------------------//
+
+void Agregar_nodo (char car) 
+     {
+		if (cabeza == NULL) 
+		   {
+			cabeza = new Nodo;
+			cola = cabeza;
+			cola->sig = NULL;
+			cabeza->ant = NULL;
+    	   } 
+	    else 
+		   {
+			cola->sig = new Nodo;
+			cola->sig->ant = cola;
+			cola = cola->sig;
+			cola->sig = NULL;
+		   }
+		cola->info = car;	
+	 }	 
+	 
+
+void Agregar_nodoR (char car) 
+     {
+		if (cabezaR == NULL) 
+		   {
+			cabezaR = new Nodo;
+			colaR = cabezaR;
+			colaR->sig = NULL;
+			cabezaR->ant = NULL;
+    	   } 
+	    else 
+		   {
+			colaR->sig = new Nodo;
+			colaR->sig->ant = colaR;
+			colaR = colaR->sig;
+			colaR->sig = NULL;
+		   }
+		colaR->info = car;
+	 }
+	 
+void Borrar_cadena () // Borra la lista ingresada
+     {
+		Nodo *temp = cabeza;
+		Nodo *eli = NULL;
+		
+		while (temp != NULL) 
+			  {
+				eli = temp;
+				temp = temp->sig;
+				delete eli;
+     	      }
+		cabeza = NULL;
+		cola = NULL;
+     }
+     
+void Borrar_resultado () // Borra resultado
+     {
+		Nodo *temp = cabezaR;
+		Nodo *eli = NULL;
+		
+		while (temp != NULL) 
+			  {
+				eli = temp;
+				temp = temp->sig;
+				delete eli;
+     	      }
+		cabezaR = NULL;
+		colaR = NULL;
+     }
+
+bool Es_vocal (char car)
+     {
+		bool vocal = true; 
+		switch (car)
+		       {
+				   case'a' : case'e' : case'i' : case'o' : case 'u' :
+				   case'A' : case'E' : case'I' : case'O' : case 'U' : break;
+				   default  : vocal = false;
+			   }
+		return vocal; 
+	 }
+	 
+void Gracias ()
+     {
+	    printf("\n\n");
+	    printf("\t==================== GRACIAS ======================\n");	 
+	 }
+	 
+void Ingresar_cadena () // procedimiento para ingresar la cadena
+	 {
+	    char car;
+	    printf("\tIngrese una cadena de caracteres y "); 
+	    printf("presione enter:\n\n\n\tCadena = ");
+		while ( (car = getchar()) != '\n' )
+		      Agregar_nodo (car); // almacenando cada caracter en un nodo 
+		printf("\n\n");
+	 }
+    
+int  Ingresar_opcion ()
+  {
+	 char opcion[100], car;
+	 int N=0, op=0;
+	
+     while (op == 0)
+         {
+	       printf("\tIngrese su opcion y presione enter: ");	
+		   while ( ( car = getchar() ) != '\n' )
+		         opcion[N++] = car;
+		   if (N == 1)
+			{ 
+			  car = opcion[0];
+			  switch (car)
+			  {
+			   case 'I' : op = 1;
+			              break;
+		       case 'M' : op = 2; 
+						  break;
+		       case 'S' : op = 3; 
+						  break;
+		       default  : op = 0;
+			  }
+		    }
+	       N = 0;
+	       printf("\n\n");
+	     }
+	 return op;
+  }
+
+void Iniciar_listas () // iniciando la lista
+     {
+		cabeza = cola = NULL; 
+		cabezaR = colaR = NULL;
+     }
+
+void Invertir ()
+     {
+		Nodo *temp = cabeza;
+		char car;
+		int cont=0;
+		
+		while (temp != NULL) 
+	        {
+			  car = temp->info;
+			  	
+			  if (Es_vocal (car))
+			   {
+				 if (cont>0)
+				  {
+					for (int i=cont-1 ; 0<=i ; i--)
+				        Agregar_nodoR (pila[i]);
+				    cont = 0;
+				  }  
+				 Agregar_nodoR (car);  
+			   }   
+			  else
+			  	 pila[cont++] = car;
+			  temp = temp->sig;
+	        } 
+	    if (cont>0)
+		 {
+		   for (int i=cont-1 ; 0<=i ; i--)
+			   Agregar_nodoR (pila[i]);
+		   cont = 0;
+		 } 
+	 }
+
+void Mostrar_cadena () // Muestra la cadena ingresada
+     {
+		Nodo *temp = cabeza;
+		Nodo *imp = NULL;
+	    printf("\tCadena = ");
+		while (temp != NULL) 
+	        {
+			  imp = temp;
+			  temp = temp->sig;
+			  printf("%c",imp->info);
+	        }
+	    printf("\n\n\n");
+	 }
+	
+void Mostrar_menu ()
+	 {
+      printf("\n\n");
+	  printf("\t###################################################\n");
+	  printf("\t#                                                 #\n"); 
+	  printf("\t#         ============================            #\n"); 
+      printf("\t#                   PROGRAMA :                    #\n");
+	  printf("\t#          ENCRIPTAMIENTO DE MENSAJES             #\n");
+	  printf("\t#         ============================            #\n");
+	  printf("\t#                                                 #\n");  
+	  printf("\t#                     MENU:                       #\n");
+	  printf("\t#                                                 #\n");
+	  printf("\t#         (I)  :  Ingresar mensaje                #\n");
+	  printf("\t#         (M)  :  Mostrar Resultado               #\n");
+	  printf("\t#         (S)  :  Salir                           #\n");
+	  printf("\t#                                                 #\n");
+	  printf("\t###################################################\n");
+	  printf("\n\n\n");
+     }
+      
+void Mostrar_resultado () // Muestra la cadena encriptada
+     {
+		Nodo *temp = colaR;
+		Nodo *imp = NULL;
+	    printf("\tCadena encriptada = ");
+		while (temp != NULL) 
+	        {
+			  imp = temp;
+			  temp = temp->ant;
+			  printf("%c",imp->info);
+	        }
+	    printf("\n\n\n");
+	 }
+	
+void Program_enc ()
+   {
+	 int opcion = 0; 
+	 bool salir = false;
+	 bool ingresado = false;
+		
+	 Iniciar_listas ();
+	 while (!salir)
+	     {
+		   Mostrar_menu ();
+		   opcion = Ingresar_opcion ();
+		   switch (opcion)
+                {
+			      case 1 : {
+							   Borrar_cadena ();
+							   Ingresar_cadena (); 
+							   ingresado = true;
+			               }
+			               break;
+			      case 2 : {
+							   if (ingresado)
+							    {
+								  Invertir ();
+								  printf("\tResultados:\n\n\n");
+							      Mostrar_cadena ();
+					              Mostrar_resultado ();
+                                  Borrar_resultado ();
+							    }
+							   else
+							      printf("\tERROR: No ha ingresado cadena alguna.\n\n\n");
+						   } 
+						   break;
+		          case 3 : {
+					           Gracias ();  
+							   salir = true; 
+						   } 
+						   break;
+			    }
+		 }
+     Borrar_cadena ();
+     Borrar_resultado ();
+   }
+	 
+//---------------------------- FIN DE LA DEFINICION ---------------------------------//
